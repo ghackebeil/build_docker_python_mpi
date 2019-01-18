593bd1f53d1b9840e818a76c30080a4ab0855db0
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2019-01-17 19:08:36 -0500

    bumping ipopt version to trigger build

--- install_scripts/ipopt.sh
+++ install_scripts/ipopt.sh
@@ -6,7 +6,7 @@ RUN echo "" && \
     echo "INSTALLING IPOPT" && \
     echo "================" && \
     echo ""
-ENV IPOPT_VERSION="3.12.9"
+ENV IPOPT_VERSION="3.12.12"
 ARG TARGET="Ipopt-${IPOPT_VERSION}"
 RUN cd /root && \
     rm -rf ${TARGET}.tgz && \
