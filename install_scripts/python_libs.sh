5a5c18082090089f33644964d7de5e4c0cf18d71
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2019-01-17 19:56:18 -0500

    matplotlib installation may fail on pypy

--- install_scripts/python_libs.sh
+++ install_scripts/python_libs.sh
@@ -16,7 +16,6 @@ RUN pip install --no-cache-dir -U \
       virtualenv \
       cffi \
       numpy \
-      matplotlib \
       mpi4py \
       cryptography \
       sympy \
@@ -28,5 +27,6 @@ RUN pip install --no-cache-dir PyYAML || \
     pip install https://github.com/yaml/pyyaml/archive/4.1.zip || \
     echo failed to install PyYAML
 RUN pip install --no-cache-dir scipy || echo failed to install scipy
+RUN pip install --no-cache-dir matplotlib || echo failed to install matplotlib
 RUN pip install --no-cache-dir numba || echo failed to install numba
 RUN pip install --no-cache-dir pandas || echo failed to install pandas
